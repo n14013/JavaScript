f677d5f8052c33e04267a63eba6ebb3a2c08a869
n14013 <n14013@std.it-college.ac.jp>
2015-06-26 14:36:12 +0900

    6/26

--- main.js
+++ main.js
@@ -1,54 +1,54 @@
-var SCREEN_SIZE = 500;                    // キャンバスの幅
-var SIDE_CELLS = 200;                     // 一辺のセルの数
-var CELL_SIZE = SCREEN_SIZE / SIDE_CELLS; // セルの幅
-var FPS = 10;                             // フレームレート
-var canvas;                     //= document.getElementById('world');
-var context;                    //= canvas.getContext('2d');
+var SCREEN_SIZE = 500;                   
+var SIDE_CELLS = 200;                   
+var CELL_SIZE = SCREEN_SIZE / SIDE_CELLS; 
+var FPS = 10;                            
+var canvas;                    
+var context;                  
 
 window.onload = function() {
-    var field = new Array(SIDE_CELLS*SIDE_CELLS); // フィールド情報
-    var tempField = new Array(SIDE_CELLS*SIDE_CELLS); // フィールド情報の一時記憶用
-    for (var i=0; i<field.length; i++) field[i] = Math.floor(Math.random()*2); // ランダムに「生」「死」を格納
-    canvas = document.getElementById('world'); // canvas要素を取得
-    canvas.width = canvas.height = SCREEN_SIZE; // キャンバスのサイズを設定
-    var scaleRate = Math.min(window.innerWidth/SCREEN_SIZE, window.innerHeight/SCREEN_SIZE); // Canvas引き伸ばし率の取得
-    canvas.style.width = canvas.style.height = SCREEN_SIZE*scaleRate+'px';  // キャンバスを引き伸ばし
-    context = canvas.getContext('2d');                // コンテキスト
-    context.fillStyle = 'rgb(211, 85, 149)';          // 色
-    update(field, tempField);   // ゲームループ開始
+    var field = new Array(SIDE_CELLS*SIDE_CELLS); 
+    var tempField = new Array(SIDE_CELLS*SIDE_CELLS); 
+    for (var i=0; i<field.length; i++) field[i] = Math.floor(Math.random()*2); 
+    canvas = document.getElementById('world'); 
+    canvas.width = canvas.height = SCREEN_SIZE; 
+    var scaleRate = Math.min(window.innerWidth/SCREEN_SIZE, window.innerHeight/SCREEN_SIZE); 
+    canvas.style.width = canvas.style.height = SCREEN_SIZE*scaleRate+'px';  
+    context = canvas.getContext('2d');                
+    context.fillStyle = 'rgb(211, 85, 149)';         
+    update(field, tempField);   
 }
 
 function update(field, tempField) {
-    var n = 0;                    // 自身のまわりにある「生」の数
-    tempField = field.slice(); // 複製
+    var n = 0;                 
+    tempField = field.slice();
     for (var i=0; i<tempField.length; i++) {
         n = 0;
         for (var s=-1; s<2; s++) {
             for (var t=-1; t<2; t++) {
-                if (s==0 && t==0) continue; // 自身はカウントしない
-                var c = i + s*SIDE_CELLS + t;   // チェックするセル
-                if (c>=0 && c<tempField.length) { // 配列からはみ出していないか(上下の壁判定も兼ねて)
-                    if (i<c && c%SIDE_CELLS!=0 || i>c && c%SIDE_CELLS!=SIDE_CELLS-1) { // 左右の壁判定
-                        if (tempField[c]) n ++; // 「生」だったらカウント
+                if (s==0 && t==0) continue; 
+                var c = i + s*SIDE_CELLS + t;   
+                if (c>=0 && c<tempField.length) { 
+                    if (i<c && c%SIDE_CELLS!=0 || i>c && c%SIDE_CELLS!=SIDE_CELLS-1) { 
+                        if (tempField[c]) n ++; 
                     }
                 }
             }
         }
-        if (tempField[i] && (n==2||n==3)) { // 自身が「生」でカウントが2か3
-            field[i] = 1;    // 「生」
-        } else if (!tempField[i] && n==3) { // 自身が「死」でカウントが3
-            field[i] = 1;    // 「生」
-        } else field[i] = 0; // 「死」
+        if (tempField[i] && (n==2||n==3)) { 
+            field[i] = 1;    
+        } else if (!tempField[i] && n==3) { 
+            field[i] = 1;    
+        } else field[i] = 0; 
     }
-    draw(field);                                    // canvasを更新
-    setTimeout(update, 1000/FPS, field, tempField); // 再帰
+    draw(field);                                    
+    setTimeout(update, 1000/FPS, field, tempField); 
 }
 
 function draw(field) {
-    context.clearRect(0, 0, SCREEN_SIZE, SCREEN_SIZE); // 画面をクリア
+    context.clearRect(0, 0, SCREEN_SIZE, SCREEN_SIZE); 
     for (var i=0; i<field.length; i++) {
-        var x = (i%SIDE_CELLS) * CELL_SIZE;             // x座標
-        var y = Math.floor(i/SIDE_CELLS) * CELL_SIZE; // y座標
-        if (field[i]) context.fillRect(x, y, CELL_SIZE, CELL_SIZE); // 「生」を描画
+        var x = (i%SIDE_CELLS) * CELL_SIZE;            
+        var y = Math.floor(i/SIDE_CELLS) * CELL_SIZE; 
+        if (field[i]) context.fillRect(x, y, CELL_SIZE, CELL_SIZE); 
     }
 }
